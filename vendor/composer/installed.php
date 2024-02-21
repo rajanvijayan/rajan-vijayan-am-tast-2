40fdb04b4bfd607b71d7b6a97bfa97ea812bab9f
Rajan Vijayan <rajanit2000@gmail.com>
2024-02-22 01:19:40 +0530

    remove vendor folder in gir ignore file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'rajan-vijayan/rajan-vijayan-plugin',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rajan-vijayan/rajan-vijayan-plugin' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
